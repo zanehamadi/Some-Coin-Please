c287e33ee853a17e52bb89f8afc9080c6e10b4d4
zaneh <zanehamadi@gmail.com>
2021-12-04 22:59:58 -0500

    modified files to ready deployment

--- backend/routes/index.ts
+++ backend/routes/index.ts
@@ -9,20 +9,20 @@ router.use('/api', apiRouter)
 
 if (isProduction) {
   const path = require('path');
-  router.get('/', (req:any, res:any) => {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.sendFile(
-      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
-    );
+  router.get('/', (req, res) => {
+      res.cookie('XSRF-TOKEN', req.csrfToken());
+      return res.sendFile(
+          path.resolve(__dirname, '../../../frontend', 'build', 'index.html')
+      );
   });
 
   router.use(express.static(path.resolve("../frontend/build")));
 
-  router.get(/^(?!\/?api).*/, (req:any, res:any) => {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.sendFile(
-      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
-    );
+  router.get(/^(?!\/?api).*/, (req, res) => {
+      res.cookie('XSRF-TOKEN', req.csrfToken());
+      return res.sendFile(
+          path.resolve(__dirname, '../../../frontend', 'build', 'index.html')
+      );
   });
 }
 
